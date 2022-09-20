7ae4ee4e6d463d1ea86887c4fce830c87ddfcbf0
Mathew Torres <torres.mathew@gmail.com>
2022-09-20 14:44:20 -0700

    update lab 1 with corrections

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -2,50 +2,15 @@ import React from "react";
 
 
 
-const animalArray =
-  [{
-    "_id": 1,
-    "image_url": "https://img.freepik.com/free-vector/cute-monster-kid-cartoon-vector-icon-illustration-monster-holiday-icon-concept-isolated-premium-vector-flat-cartoon-style_138676-3995.jpg?w=740&t=st=1663633510~exp=1663634110~hmac=c3fcf0c65974c6053fe65812a57947376359f9b49f43454e9e8678893ca7e53e", 
-    "title": "Horned Animal 1",
-    "description": "A unicorn and a narwhal nuzzling their horns",
-    "keyword": "narwhal",
-    "horns": 1
-  },
-
-    {
-      "_id": 2,
-      "image_url": "https://img.freepik.com/premium-vector/cute-alien-unicorn-meditating-yoga-cartoon-vector-icon-illustration-science-sport-icon-isolated_138676-5900.jpg?w=740",
-      "title": "Horned Animal 2",
-      "description": "Parent rhino with two babies",
-      "keyword": "rhino",
-      "horns": 2
-    },
-
-    {
-      "_id": 3,
-      "image_url": "https://img.freepik.com/free-vector/cute-cat-unicorn-flying-cartoon-illustration-animal-wildlife-icon-concept_138676-1909.jpg?w=740&t=st=1663634825~exp=1663635425~hmac=17f30630365bf60534c07104b2f42fda54eb7838aed70fe334961a33ca8c9300",
-      "title": "Horned Animal 3",
-      "description": "Someone wearing a very silly unicorn head mask",
-      "keyword": "unicorn",
-      "horns": 1
-    }]
 
 class HornedBeast extends React.Component {
   render() {
     return (
       <>
 
-        {animalArray.map((animal, index) => {
-
-          return (
-            <>
-              <h2>{animal.title}</h2>
-              <img src={animal.image_url} alt={animal.title} title={animal.title} />
-              <p>{animal.description}</p>
-            </>
-          )
-        })}
-
+        <h2>{this.props.title}</h2>
+        <img src={this.props.image_url} alt={this.props.title} title={this.props.title} />
+        <p>{this.props.description}</p>
       </>
     );
   }
