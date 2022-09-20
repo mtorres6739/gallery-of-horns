b2ed815fef6f4ebbe928882bfcc9a6240aae7e5e
Mathew Torres <torres.mathew@gmail.com>
2022-09-19 17:57:57 -0700

    update gallery of horns lab

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <>
+        <h1> &copy; Mathew Torres </h1>
+      </>
+    );
+  }
+}
+
+export default Footer;
