ef14ee9df8f9273654229dc5a873da1bbdda8f0f
Mathew Torres <torres.mathew@gmail.com>
2022-09-22 16:03:38 -0700

    completes lab 4

--- src/Main.js
+++ src/Main.js
@@ -10,6 +10,7 @@ import Col from 'react-bootstrap/Col';
 
 // import data.json
 import data from "./assets/data.json";
+import BeastForm from "./components/BeastForm";
 
 
 class Main extends React.Component {
@@ -19,6 +20,7 @@ class Main extends React.Component {
     this.state = {
       showModel: false,
       selectedBeast: {},
+      displayedBeast: data,
     };
   }
 
@@ -36,36 +38,63 @@ class Main extends React.Component {
   }
 
 
+  // handle form submit for DESCRIPTION
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // const searchTerm = event.target.description.value;
+    // const filteredBeast = data.filter((beast) => beast.description.toLocaleLowerCase().includes(searchTerm.toLocaleLowerCase()));
+    // this.setState({ displayedBeast: filteredBeast });
+
+    const searchHorns = event.target.horns.value;
+    const filteredHorns = data.filter((beast) => beast.horns === parseInt(searchHorns));
+    this.setState({ displayedBeast: filteredHorns });
+  };
+
+// handle form submit for HORNS
+  // handleFormSubmitHorns = (event) => {
+  //   event.preventDefault();
+  //   const searchHorns = event.target.horns.value;
+  //   const filteredHorns = data.filter((beast) => beast.horns === parseInt(searchHorns));
+  //   this.setState({ displayedBeast: filteredHorns });
+  // };
 
   render() {
     return (
-      <Container>
-        <Row>
-          {data.map((beast, index) => {
-            return (
-              <Col>
-              <HornedBeast
-                key={beast._id}
-                id={beast._id}
-                title={beast.title}
-                description={beast.description}
-                image_url={beast.image_url}
-                horns={beast.horns}
-                showModalTrue={this.setShowModelTrue}
-              />
-              </Col>
-            );
-          })}
+      <>
+        <BeastForm handleFormSubmit={this.handleFormSubmit} />
 
 
-          <BeastModal
-            showModel={this.state.showModel}
-            setShowModelFalse={this.setShowModelFalse}
-            selectedBeast={this.state.selectedBeast}
-          />
 
-        </Row>
-      </Container>
+        <Container>
+          <Row>
+            {/* {data.map((beast, index) => { */}
+            {this.state.displayedBeast.map((beast) => {
+              return (
+                <Col>
+                  <HornedBeast
+                    key={beast._id}
+                    id={beast._id}
+                    title={beast.title}
+                    description={beast.description}
+                    image_url={beast.image_url}
+                    horns={beast.horns}
+                    showModalTrue={this.setShowModelTrue}
+                  />
+                </Col>
+              );
+            })}
+
+
+            <BeastModal
+              showModel={this.state.showModel}
+              setShowModelFalse={this.setShowModelFalse}
+              selectedBeast={this.state.selectedBeast}
+            />
+
+          </Row>
+        </Container>
+      </>
     );
   }
 }
